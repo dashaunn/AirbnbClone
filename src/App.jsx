cad4ed7d60b305085bb012a19f472ee1af180279
Dashaun Sutton-Harris <114096734+dashaunn@users.noreply.github.com>
2024-01-15 18:07:59 -0600

    update App component to return the Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import { useState } from 'react'
 import './App.css'
+import Navbar from './components/Navbar'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <h1>App component</h1>
-    </>
+    <div>
+      
+      <Navbar />
+
+    </div>
   )
 }
 
